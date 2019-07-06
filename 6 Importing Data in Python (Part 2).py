14737cc9ee6836a9b71580fe8f135d8c5c36b826
Ran Dou <randou.brandeis@gmail.com>
2019-07-06 11:16:31 -0400

    Finish the 6th course

--- 6 Importing Data in Python (Part 2).py
+++ 6 Importing Data in Python (Part 2).py
@@ -58,7 +58,7 @@ for link in soup.find_all('a'):
     print(link.get('href'))
 
 # =============================================================================
-# APT
+# API
 # =============================================================================
 
 # Application Programming Interface: allows two software programs to communicate with each other
@@ -88,6 +88,88 @@ r = requests.get(url)
 json_data = r.json()
 pizza_extract = json_data['query']['pages']['24768']['extract']
 print(pizza_extract)
+
+### tweepy Package
+# REST APIs: Representational State Transfer, read and write Twitter data
+# Streaming APIs (Public streams) - GET statuses/sample API
+#                                 - Firehouse API get you to all public data
+import tweepy, json
+# pass the API Key and Secret to the handler and pass to access credentials using the set_acess_token method
+access_token = "1092294848-aHN7DcRP9B4VMTQIhwqOYiB14YkW92fFO8k8EPy"
+access_token_secret = "X4dHmhPfaksHcQ7SCbmZa2oYBBVSD2g8uIHXsp5CTaksx"
+consumer_key = "nZ6EA0FxZ293SxGNg8g8aP0HM"
+consumer_secret = "fJGEodwe3KiKUnsYJC3VRndj7jevVvXbK2D5EiJ2nehafRgA6i"
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+# define stream listener class
+# Template
+class MyStreamListener(tweepy.StreamListener):
+    def __init__(self, api=None):
+        super(MyStreamListener, self).__init__()
+        self.num_tweets = 0
+        self.file = open("tweets.txt", "w")
+    
+    def on_status(self, status):
+        tweet = status._json
+        self.file.write(json.dumps(tweet) + '\n')
+        tweet_list.append(status)
+        self.num_tweets += 1
+        if self.num_tweets < 100:
+            return True
+        else:
+            return False
+        self.file.close()
+ # Create Streaming object and authenticate
+l = MyStreamListener()
+stream = tweepy.Stream(auth, l)
+# This line filters Twitter Streams to capture data by keywords:
+stream.filter(track=['apples', 'oranges'])
+# String of path to file: tweets_data_path
+tweets_data_path = 'tweets3.txt'
+tweets_data = []
+tweets_file = open(tweets_data_path, "r")
+# Read in tweets and store in list: tweets_data
+for line in tweets_file:
+    tweet = json.loads(line)
+    tweets_data.append(tweet)
+tweets_file.close()
+print(tweets_data[0].keys())
+df = pd.DataFrame(tweets_data, columns=['text', 'lang'])
+print(df.head())
+
+### Twitter text analysis
+import re
+def word_in_text(word, text):
+    word = word.lower()
+    text = tweet.lower()
+    match = re.search(word, text)
+    if match:
+        return True
+    return False
+# Initialize list to store tweet counts
+[clinton, trump, sanders, cruz] = [0, 0, 0, 0]
+# Iterate through df, counting the number of tweets in which
+# each candidate is mentioned
+for index, row in df.iterrows():
+    clinton += word_in_text('clinton', row['text'])
+    trump += word_in_text('trump', row['text'])
+    sanders += word_in_text('sanders', row['text'])
+    cruz += word_in_text('cruz', row['text'])
+# Data Visualization
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.set(color_codes=True)
+cd = ['clinton', 'trump', 'sanders', 'cruz']
+ax = sns.barplot(cd, [clinton, trump, sanders, cruz])
+ax.set(ylabel="count")
+plt.show()
+
+
+
+
+
+
+
 
 
 
