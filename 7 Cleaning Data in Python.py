6019ea6dd3067d09935e3a3fdea5d48b4176c6a4
Ran Dou <randou.brandeis@gmail.com>
2019-07-07 21:28:26 -0400

    Finish the 7th course

--- 7 Cleaning Data in Python.py
+++ 7 Cleaning Data in Python.py
@@ -186,8 +186,65 @@ assert (ebola >= 0).all().all()
 # =============================================================================
 
 
-
-
+import pandas as pd
+gapminder = pd.read_csv('gapminder.csv')
+gapminder_melt = gapminder.melt(id_vars='Life expectancy')
+gapminder_melt.columns = ['country','year','life_expectancy']
+print(gapminder_melt.head())
+# Convert the year column to numeric
+gapminder.year = pd.to_numeric(gapminder.year)
+# Test if country is of type object
+assert gapminder.country.dtypes == np.object
+# Test if year is of type int64
+assert gapminder.year.dtypes == np.int64
+# Test if life_expectancy is of type float64
+assert gapminder.life_expectancy.dtypes == np.float64
+# Create the series of countries: countries
+countries = gapminder.country
+# Drop all the duplicates from countries
+countries = countries.drop_duplicates()
+# Write the regular expression: pattern
+pattern = '^[A-Za-z\.\s]*$'
+# Create the Boolean vector: mask
+mask = countries.str.contains(pattern)
+# Invert the mask: mask_inverse: True to False, False to True
+mask_inverse = ~mask
+# Subset countries using mask_inverse: invalid_countries
+invalid_countries = countries.loc[mask_inverse]
+# Print invalid_countries
+print(invalid_countries)
+# Assert that country does not contain any missing values
+assert pd.notnull(gapminder.country).all()
+# Assert that year does not contain any missing values
+assert gapminder.year.notnull().all()
+# Drop the missing values
+gapminder = gapminder.dropna()
+# Print the shape of gapminder
+print(gapminder.shape)
+# Add first subplot
+plt.subplot(2, 1, 1) 
+# Create a histogram of life_expectancy
+gapminder['life_expectancy'].plot(kind='hist')
+# Group gapminder: gapminder_agg
+gapminder_agg = gapminder.groupby('year')['life_expectancy'].mean()
+# Print the head of gapminder_agg
+print(gapminder_agg.head())
+# Print the tail of gapminder_agg
+print(gapminder_agg.tail())
+# Add second subplot
+plt.subplot(2, 1, 2)
+# Create a line plot of life expectancy per year
+gapminder_agg.plot()
+# Add title and specify axis labels
+plt.title('Life expectancy over the years')
+plt.ylabel('Life expectancy')
+plt.xlabel('Year')
+# Display the plots
+plt.tight_layout()
+plt.show()
+# Save both DataFrames to csv files
+gapminder.to_csv('gapminder.csv')
+gapminder_agg.to_csv('gapminder_agg.csv')
 
 
 
