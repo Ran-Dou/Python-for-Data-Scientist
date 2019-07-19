ae2b0ff17f8ab353cdf0d90b91a74b8e7d5057dc
Ran Dou <randou.brandeis@gmail.com>
2019-07-18 21:32:04 -0400

    Finish the first section of the 11th course

--- 11 Analyzing Police Activity with pandas.py
+++ 11 Analyzing Police Activity with pandas.py
@@ -1,8 +1,13 @@
 import os
 os.chdir('/Users/randou/Esther/Coding/Python/Data Scientist with Python/Python-for-Data-Scientist')
 
-
-
+# trick
+df.isnull().sum() # number of missing value in each row
+df.drop('colname', axis='columns', inplace=True)
+df.dropna(subset=['col1', 'col2'], inplace=True)
+df.col1.str.cat(df.col2, sep=' ') # combine two columns
+df.col.dtype
+pd.to_datetime
 
 
 
