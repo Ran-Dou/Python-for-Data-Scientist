bea708f842bfe03cd9c8f5f50d8a69e7479ab83c
Ran <43659207+Ran-Dou@users.noreply.github.com>
2019-10-17 23:59:33 -0400

    Update 15 Interactive Data Visualization with Bokeh.py

--- 15 Interactive Data Visualization with Bokeh.py
+++ 15 Interactive Data Visualization with Bokeh.py
@@ -8,7 +8,7 @@ os.chdir('/Users/randou/Esther/Coding/Python/Data Scientist with Python/Python-f
 ### Glyphs
 # visual shapes, with properties attached to data
 from bokeh.io import output_file, show
-from bokeh.plotting import figure
+from bokeh.plotting import figure 
 plot = figure(plot_width=400, tools='pan,box_zoom')
 plot.circle([1,2,3,4,5],[8,6,5,2,3])
 output_file('circle.html')
